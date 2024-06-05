6c2463fde2003d4f3a526bf1a2d3de5bb3925742
ABDULLAH ADAILEH <169062688+abdullahadaileh@users.noreply.github.com>
2024-06-05 12:45:39 +0300

    Add files via upload

---
+++ JS 3ed task/index.js
@@ -0,0 +1,74 @@
+// Task11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+
+function sayHello(name) {
+    console.log("Hello " + name);
+}
+sayHello("Alice");
+sayHello("Bob");
+
+// Task222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+
+let square = function (num) {
+    return num**2;
+}
+console.log(square(2));
+console.log(square(5));
+
+// // sol 222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+// let square = function (num) {
+//     return num =num*num
+// }
+// console.log(square(5));
+
+// function square(num) {
+//     return num*num
+// }
+// console.log(square(4));
+
+// task3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+function multiply(x,y) {
+    return x*y;
+}
+console.log(multiply(2,3));
+console.log(multiply(4,5));
+
+// Task4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+
+let car =
+{
+    make: "Toyota", model:"Camry", 
+   year:"2020",
+    getCarInfo: function() {
+        return car.mack="Toyota Camry" , car.make.year="Toyota Camry 2020";
+    }
+}
+console.log(car.make);
+console.log(car.getCarInfo());
+
+// Task55555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+
+var colors= [ "red","green","blue"]
+for(let i in colors){ 
+    console.log(colors[i])
+}
+
+// Task66666666666666666666666666666666666666666666665556666666666666666666666666666666666666666666666666666666666666
+
+
+function getStringLength(x) {
+    return x.length;
+}
+console.log(getStringLength("hello world"));
+
+// Task77777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+
+function UpperCase(x) {
+    return x.toUpperCase();
+}
+console.log(UpperCase("hello"));
+console.log(UpperCase("orange"));
+
+
+
+
