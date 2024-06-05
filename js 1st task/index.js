77360e0d5462c63e04bdafff4dfe80b1f2c03a24
ABDULLAH ADAILEH <169062688+abdullahadaileh@users.noreply.github.com>
2024-06-05 12:40:45 +0300

    Add files via upload

---
+++ js 1st task/index.js
@@ -0,0 +1,51 @@
+
+// task1   task1   task1   task1   task1
+
+let score =prompt("test");
+let grado
+if (Number(score) <= 50){
+    grado ="you failed the exam";
+}
+alert(`${grado}`);
+
+// task2   task2   task2   task2   task2
+
+let Name = prompt("name");
+let grade;
+if (Name == "jhone"){
+    grade = "Hello jhone";
+}
+alert(`${grade}`);
+
+// task3   task3   task3   task3   task3
+
+let Day = prompt("Day");
+let grode;
+if (Day =="Monday") {
+    grode = "It's a weekday";
+}
+if ("Friday"+"Saturday" != "Monday");
+alert(`${grode}`);
+
+// task4   task4   task4   task4   task4
+
+for (let i =0; i < 10; i++)
+     {
+        console.log()
+     }
+
+// task5   task5   task5   task5   task5
+
+for (i=1;i<=10;i++)
+    {
+        console.log(i);
+    }
+
+
+
+
+
+
+
+
+
