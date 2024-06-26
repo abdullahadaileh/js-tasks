b4aeef3122cad5fbf220b68a5a26a36ee4c8619e
abdullahadaileh <abdullahadaileh957@gmail.com>
2024-06-26 11:48:06 +0300

    hee

--- js-week3-day-3/index.js
+++ js-week3-day-3/index.js
@@ -1,39 +1,36 @@
-function fetchData() {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-  
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-      })
-      .then(data => {
-        displayPosts(data);
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-      });
-  }
 
-  function displayPosts(posts) {
-    const postsContainer = document.getElementById('posts');
-    
-    posts.forEach(post => {
-      const postDiv = document.createElement('div');
-      postDiv.className = 'post';
-      
-      const postTitle = document.createElement('h3');
-      postTitle.textContent = post.title;
-      
-      const postBody = document.createElement('p');
-      postBody.textContent = post.body;
-      
-      postDiv.appendChild(postTitle);
-      postDiv.appendChild(postBody);
-      
-      postsContainer.appendChild(postDiv);
-    });
-  }
-    fetchData();
-  
+
+
+
+
+let container = document.getElementById('posts');
+
+function fetchData(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(Response => Response.json() )
+    .then(data=>{
+        let template = '';
+        data.forEach(posts =>{
+            template+=
+            `
+            <div class="post">
+            <h3>${posts.title}</h3>
+            <h3>${posts.body}</h3>
+            </div>
+            `
+        });
+        container.innerHTML = template;
+    })
+}
+
+fetchData();
+
+
+
+
+
+
+
+
+
+
